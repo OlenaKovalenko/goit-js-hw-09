4adb0ad433499e2a746d14ee752a2032ec003ce4
OlenaKovalenko <elenkow17@gmail.com>
2023-09-14 23:23:12 +0300

    Update task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const refs = {
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+    body: document.querySelector('body'),
+}
 
+refs.startBtn.addEventListener('click', onStartClick);
+refs.stopBtn.addEventListener('click', onStopClick);
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+let id = null;
+
+function onStartClick() {
+    refs.startBtn.setAttribute("disabled", "disabled");
+    id = setInterval(() => {
+        refs.body.style.backgroundColor = getRandomHexColor();
+    }, 1000)
+    
+}
+
+function onStopClick() {
+    clearInterval(id);
+    refs.startBtn.removeAttribute("disabled");
+}
